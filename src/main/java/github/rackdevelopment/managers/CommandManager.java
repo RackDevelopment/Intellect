5c2ad27f030cb21a95ebaab1ce6803001fd009aa
Rack <22134468+RackDevelopment@users.noreply.github.com>
2020-12-02 17:37:23 -0500

    Re-upload

---
+++ src/main/java/github/rackdevelopment/managers/CommandManager.java
@@ -0,0 +1,13 @@
+package github.rackdevelopment.managers;
+
+import github.rackdevelopment.Intellect;
+
+public class CommandManager {
+
+    private final Intellect intellect;
+
+    public CommandManager(Intellect intellect) {
+        this.intellect = intellect;
+    }
+
+}
